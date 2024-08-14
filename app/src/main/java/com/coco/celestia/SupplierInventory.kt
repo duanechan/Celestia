e08dc83323a88678ae02f9dd13fc43654e81aa48
EARL JEMS CONSTANTINO <2222765@slu.edu.ph>
2024-08-14 21:25:28 +0800

    Modified rows to card at SupplierInventory

--- app/src/main/java/com/coco/celestia/SupplierInventory.kt
+++ app/src/main/java/com/coco/celestia/SupplierInventory.kt
@@ -2,10 +2,15 @@ package com.coco.celestia
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
@@ -35,6 +40,7 @@ fun SupplierInventory(){
         modifier = Modifier
             .fillMaxSize()
             .padding(top = 75.dp)
+            .verticalScroll(rememberScrollState())
     ){
         Row(modifier = Modifier
             .fillMaxWidth()
@@ -61,37 +67,64 @@ fun SupplierInventory(){
                 onActiveChange = {},
                 placeholder = { Text(text = "Search...", color = Color.Black, fontSize = 15.sp)},
                 leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")},
-                modifier = Modifier.width(225.dp).height(35.dp)){
+                modifier = Modifier
+                    .width(225.dp)
+                    .height(35.dp)){
                 //TO DO
             }
         }
 
         Spacer(modifier = Modifier.height(10.dp))
 
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .height(225.dp)
-            .background(LightGreen)
-            .padding(top = 27.dp, bottom = 8.dp, start = 25.dp, end = 16.dp)){
-            Text(text = "Item 1", fontSize = 31.sp, fontWeight = FontWeight.Bold, color = Color.White)
+        Card(modifier = Modifier
+            .width(500.dp)
+            .height(200.dp)
+            .offset(x = (-16).dp, y = 0.dp)
+            .padding(top = 0.dp, bottom = 5.dp, start = 30.dp, end = 0.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = LightGreen
+            )){
+            Text(text = "Item 1", fontSize = 29.sp, fontWeight = FontWeight.Bold, color = Color.White,
+                modifier = Modifier.padding(top = 15.dp, start = 10.dp))
         }
 
         Spacer(modifier = Modifier.height(10.dp))
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .height(225.dp)
-            .background(LightGreen)
-            .padding(top = 27.dp, bottom = 8.dp, start = 25.dp, end = 16.dp)){
-            Text(text = "Item 2", fontSize = 31.sp, fontWeight = FontWeight.Bold, color = Color.White)
+        Card(modifier = Modifier
+            .width(500.dp)
+            .height(200.dp)
+            .offset(x = (-16).dp, y = 0.dp)
+            .padding(top = 0.dp, bottom = 5.dp, start = 30.dp, end = 0.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = LightGreen
+            )){
+            Text(text = "Item 2", fontSize = 29.sp, fontWeight = FontWeight.Bold, color = Color.White,
+                modifier = Modifier.padding(top = 15.dp, start = 10.dp))
         }
 
         Spacer(modifier = Modifier.height(10.dp))
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .height(225.dp)
-            .background(LightGreen)
-            .padding(top = 27.dp, bottom = 8.dp, start = 25.dp, end = 16.dp)){
-            Text(text = "Item 3", fontSize = 31.sp, fontWeight = FontWeight.Bold, color = Color.White)
+        Card(modifier = Modifier
+            .width(500.dp)
+            .height(200.dp)
+            .offset(x = (-16).dp, y = 0.dp)
+            .padding(top = 0.dp, bottom = 5.dp, start = 30.dp, end = 0.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = LightGreen
+            )){
+            Text(text = "Item 3", fontSize = 29.sp, fontWeight = FontWeight.Bold, color = Color.White,
+                modifier = Modifier.padding(top = 15.dp, start = 10.dp))
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+        Card(modifier = Modifier
+            .width(500.dp)
+            .height(200.dp)
+            .offset(x = (-16).dp, y = 0.dp)
+            .padding(top = 0.dp, bottom = 5.dp, start = 30.dp, end = 0.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = LightGreen
+            )){
+            Text(text = "Item 4", fontSize = 29.sp, fontWeight = FontWeight.Bold, color = Color.White,
+                modifier = Modifier.padding(top = 15.dp, start = 10.dp))
         }
 
     }
