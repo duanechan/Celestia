5407d64ffc924fa1e0071a8e797cc7aadc4a07fd
MICHAEL JOHN P. NGAYAWON <2222574@slu.edu.ph>
2024-10-15 21:11:36 +0800

    Minor Editions from Previous Commit

--- app/src/main/java/com/coco/celestia/screens/admin/AdminInventory.kt
+++ app/src/main/java/com/coco/celestia/screens/admin/AdminInventory.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
@@ -29,7 +28,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -68,9 +66,8 @@ fun AdminInventory(
             "Admin"
         )
     }
-    val context = LocalContext.current
     LaunchedEffect(orderData, productData) {
-        monthlyInventory = calculateMonthlyInventory(context, orderData, productData)
+        monthlyInventory = calculateMonthlyInventory(orderData, productData)
     }
     BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
         Column(
@@ -220,7 +217,7 @@ fun AdminItemCard(productName: String, quantity: Int) {
         }
     }
 }
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun TopBarInventory(navController: NavController, onTabSelected: (String) -> Unit) {
     var selectedOption  by remember { mutableIntStateOf(0) }
--- app/src/main/java/com/coco/celestia/util/CalculateMonthlyInventory.kt
+++ app/src/main/java/com/coco/celestia/util/CalculateMonthlyInventory.kt
@@ -1,19 +1,16 @@
 package com.coco.celestia.util
 
-import android.content.Context
-import android.widget.Toast
 import com.coco.celestia.viewmodel.model.MonthlyInventory
 import com.coco.celestia.viewmodel.model.OrderData
 import com.coco.celestia.viewmodel.model.ProductData
 import java.time.LocalDate
 import java.time.YearMonth
 
-fun calculateMonthlyInventory(context: Context, orderData: List<OrderData>, productData: List<ProductData>): List<MonthlyInventory> {
+fun calculateMonthlyInventory(orderData: List<OrderData>, productData: List<ProductData>): List<MonthlyInventory> {
     val currentMonth = YearMonth.now()
     val thisMonthOrders = orderData.filter {
         YearMonth.from(LocalDate.parse(it.orderDate)) == currentMonth
     }
-    Toast.makeText(context, "This month's orders: $thisMonthOrders", Toast.LENGTH_SHORT).show()
 
     val orderedQuantities = thisMonthOrders
         .groupBy { it.orderData.name }
