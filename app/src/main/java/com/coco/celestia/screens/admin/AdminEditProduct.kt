215a6ee15ff6d5476d67274dc171c83c58569759
MICHAEL JOHN P. NGAYAWON <2222574@slu.edu.ph>
2024-11-08 19:35:55 +0800

    Added change product image button in admin edit product form

--- app/src/main/java/com/coco/celestia/screens/admin/AdminEditProduct.kt
+++ app/src/main/java/com/coco/celestia/screens/admin/AdminEditProduct.kt
@@ -1,5 +1,15 @@
+@file:OptIn(ExperimentalCoilApi::class)
+
 package com.coco.celestia.screens.admin
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,14 +19,22 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
+import androidx.core.content.ContextCompat
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.coco.celestia.R
+import com.coco.celestia.components.toast.ToastStatus
+import com.coco.celestia.service.ImageService
 import com.coco.celestia.viewmodel.ProductViewModel
 import com.coco.celestia.viewmodel.TransactionViewModel
 import com.coco.celestia.viewmodel.model.ProductData
@@ -39,12 +57,58 @@ fun EditProduct(
     val formattedDateTime = currentDateTime.format(formatter).toString()
     val productName by remember { mutableStateOf(productData.name) }
     var updatedPrice by remember { mutableStateOf(productData.priceKg.toString()) }
+    val context = LocalContext.current
+    var productImage by remember { mutableStateOf<Uri?>(null) }
+    var updatedProductImage by remember { mutableStateOf<Uri?>(null) }
+    val galleryLauncher = rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) {
+        updatedProductImage = it
+    }
+    var toastEvent by remember { mutableStateOf(Triple(ToastStatus.INFO, "", 0L)) }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted ->
+            if (isGranted) {
+                galleryLauncher.launch("image/*")
+            } else {
+                toastEvent = Triple(
+                    ToastStatus.WARNING,
+                    "Grant app access to add product image.",
+                    System.currentTimeMillis()
+                )
+            }
+        }
+    )
+
+    fun openGallery () {
+        val permission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            Manifest.permission.READ_MEDIA_IMAGES
+        } else {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        }
+
+        when (ContextCompat.checkSelfPermission(context, permission)) {
+            PackageManager.PERMISSION_GRANTED -> {
+                galleryLauncher.launch("image/*")
+            }
+
+            else -> {
+                permissionLauncher.launch(permission)
+            }
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        ImageService.fetchProfilePicture(productName) {
+            productImage = it
+        }
+    }
 
     AlertDialog(
         onDismissRequest = { onDismiss() },
         title = {
             Text(
-                text = "Change $productName Price/Kg",
+                text = "Edit $productName",
                 modifier = Modifier.semantics { testTag = "android:id/dialogTitle" }
             )
         },
@@ -59,6 +123,21 @@ fun EditProduct(
                         .semantics { testTag = "android:id/priceInputField" }
                 )
                 Spacer(modifier = Modifier.height(8.dp))
+
+                Button(
+                    onClick = {
+                        openGallery()
+                    }
+                ) {
+                    Text("Change Product Image")
+                }
+
+                Image(
+                    painter = rememberImagePainter(
+                        data = updatedProductImage ?: productImage ?: R.drawable.profile_icon
+                    ),
+                    contentDescription = "Product Image"
+                )
             }
         },
         confirmButton = {
@@ -74,6 +153,15 @@ fun EditProduct(
                         )
                     )
                     productViewModel.updateProductPrice(productName, updatedPrice.toDouble())
+                    updatedProductImage?.let {
+                        ImageService.uploadProductPicture(productName, it) { status ->
+                            if (status) {
+                                Log.d("ProfileScreen", "Product image uploaded successfully!")
+                            } else {
+                                Log.d("ProfileScreen", "Product image upload failed!")
+                            }
+                        }
+                    }
                     onDismiss()
                 },
                 modifier = Modifier.semantics { testTag = "android:id/saveButton" }
