13ced0e75756cec39acd99181f0cff65131b1673
ronsaludares <147226933+ronsaludares@users.noreply.github.com>
2025-01-01 16:29:29 +0800

    added initial action buttons in client dashboard(wip)

--- app/src/main/java/com/coco/celestia/screens/client/ClientDashboard.kt
+++ app/src/main/java/com/coco/celestia/screens/client/ClientDashboard.kt
@@ -5,87 +5,45 @@ import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.coco.celestia.R
-import com.coco.celestia.screens.`object`.Screen
-import com.coco.celestia.service.NotificationService
-import com.coco.celestia.ui.theme.BABText
-import com.coco.celestia.ui.theme.BAButton
-import com.coco.celestia.ui.theme.CLGText
-import com.coco.celestia.ui.theme.ClientBG
-import com.coco.celestia.ui.theme.ContainerLO
-import com.coco.celestia.ui.theme.CDText
-import com.coco.celestia.ui.theme.CDarkOrange
-import com.coco.celestia.ui.theme.LGContainer
-import com.coco.celestia.ui.theme.SoftCOrange
-import com.coco.celestia.ui.theme.White1
+import com.coco.celestia.ui.theme.*
 import com.coco.celestia.viewmodel.OrderState
 import com.coco.celestia.viewmodel.OrderViewModel
 import com.coco.celestia.viewmodel.ProductViewModel
 import com.coco.celestia.viewmodel.TransactionViewModel
 import com.coco.celestia.viewmodel.UserViewModel
 import com.coco.celestia.viewmodel.model.Notification
-import com.coco.celestia.viewmodel.model.OrderData
-import com.coco.celestia.viewmodel.model.ProductData
 import com.coco.celestia.viewmodel.model.UserData
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.delay
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 
 @Composable
 fun ClientDashboard(
@@ -97,57 +55,33 @@ fun ClientDashboard(
 ) {
     val uid = FirebaseAuth.getInstance().uid.toString()
     val userData by userViewModel.userData.observeAsState(UserData())
-    val orderData by orderViewModel.orderData.observeAsState(emptyList())
-    val orderState by orderViewModel.orderState.observeAsState(OrderState.LOADING)
-    val featuredProducts by productViewModel.featuredProducts.observeAsState(emptyList())
-    var notifications = remember { mutableListOf<Notification>() }
     val context = LocalContext.current
+    var notifications = remember { mutableListOf<Notification>() }
     var showDialog by remember { mutableStateOf(false) }
+    var showRequestPopup by remember { mutableStateOf(false) }
 
     LaunchedEffect(Unit) {
-        NotificationService.pushNotifications(
-            uid = uid,
-            onComplete = {
-                notifications.clear()
-                notifications.addAll(it)
-            },
-            onError = {
-
-            }
-        )
-        Log.d("Notifs", notifications.toString())
         orderViewModel.fetchAllOrders("", "Client")
-        Log.d("OrderData", "Observed orders: ${orderData.size}")
-        delay(1000)
-    }
-
-    LaunchedEffect(Unit) {
         productViewModel.fetchFeaturedProducts()
+        delay(1000)
     }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(White1)
-            .semantics { testTag = "android:id/ClientDashboardScreen" }
     ) {
         Column(
             modifier = Modifier
                 .fillMaxHeight()
                 .verticalScroll(rememberScrollState())
                 .padding(top = 0.dp)
-                .semantics { testTag = "android:id/ClientDashboardColumn" }
         ) {
+            // Welcome Text and Notification Button
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(
-                        top = 27.dp,
-                        bottom = 8.dp,
-                        start = 25.dp,
-                        end = 25.dp
-                    )
-                    .semantics { testTag = "android:id/DashboardHeaderRow" },
+                    .padding(top = 27.dp, bottom = 8.dp, start = 25.dp, end = 25.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 userData.let { user ->
@@ -156,9 +90,7 @@ fun ClientDashboard(
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Bold,
                         color = CDText,
-                        modifier = Modifier
-                            .weight(1f)
-                            .semantics { testTag = "android:id/WelcomeText" }
+                        modifier = Modifier.weight(1f)
                     )
 
                     Box(
@@ -183,7 +115,7 @@ fun ClientDashboard(
                         ) {
                             Box(
                                 modifier = Modifier
-                                    .background(LGContainer)
+                                    .background(Color.LightGray)
                                     .padding(17.dp)
                             ) {
                                 Image(
@@ -196,20 +128,126 @@ fun ClientDashboard(
                     }
                 }
             }
+
             Divider(
                 color = Color.Gray.copy(alpha = 0.3f),
                 thickness = 1.dp,
                 modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
             )
-            Spacer(modifier = Modifier.height(12.dp))
 
             Spacer(modifier = Modifier.height(12.dp))
 
-
+            // Row of Box Buttons
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                BoxButton(text = "Request an Order", onClick = { showRequestPopup = true })
+                BoxButton(text = "View Order History", onClick = {})
+                BoxButton(text = "View Contacts", onClick = {})
+                BoxButton(text = "To Receive", onClick = {})
+            }
+            Spacer(modifier = Modifier.height(12.dp))
         }
+    }
+
+    if (showRequestPopup) {
+        AlertDialog(
+            onDismissRequest = { showRequestPopup = false },
+            title = {
+                Column(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = "Close",
+                            modifier = Modifier
+                                .align(Alignment.TopEnd)
+                                .clickable { showRequestPopup = false }
+                                .padding(8.dp),
+                            tint = Green1
+                        )
+                    }
+                    Text(
+                        text = "How would you like to order? Choose between smaller amounts or bulk quantities.",
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = mintsansFontFamily,
+                        fontSize = 18.sp,
+                        color = Green1,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp, vertical = 8.dp)
+                    )
+                }
+            },
+            text = {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceAround,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp)
+                    ) {
+                        Text(
+                            text = "Standard Order",
+                            modifier = Modifier
+                                .clickable {
+                                    Toast.makeText(context, "Retail Order Selected", Toast.LENGTH_SHORT).show()
+                                    showRequestPopup = false
+                                }
+                                .padding(8.dp),
+                            fontFamily = mintsansFontFamily,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 15.sp,
+                            color = Green1
+                        )
+                        Text(
+                            text = "Bulk Order",
+                            modifier = Modifier
+                                .clickable {
+                                    Toast.makeText(context, "Bulk Order Selected", Toast.LENGTH_SHORT).show()
+                                    showRequestPopup = false
+                                }
+                                .padding(8.dp),
+                            fontFamily = mintsansFontFamily,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 15.sp,
+                            color = Green1
+                        )
+                    }
+                }
+            },
+            confirmButton = {}
+        )
     }
     if (showDialog) {
         ClientNotification(notifications = notifications, onDismiss = { showDialog = false })
+    }
+}
+
+@Composable
+fun BoxButton(text: String, onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .size(80.dp)
+            .background(Green2, shape = RoundedCornerShape(8.dp))
+            .clickable { onClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            color = Color.White,
+            fontSize = 12.sp,
+            textAlign = TextAlign.Center
+        )
     }
 }
 
