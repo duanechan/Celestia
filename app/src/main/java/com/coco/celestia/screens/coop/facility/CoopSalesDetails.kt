f23aa853bf002afd964db14201fcd62da4ffff8f
Jillah <alingayjillah@gmail.com>
2025-01-12 14:02:44 +0800

    Added Coop Sales Retail Order Details

--- app/src/main/java/com/coco/celestia/screens/coop/facility/CoopSalesDetails.kt
+++ app/src/main/java/com/coco/celestia/screens/coop/facility/CoopSalesDetails.kt
@@ -11,6 +11,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Card
@@ -18,7 +22,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,8 +31,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -121,8 +124,241 @@ fun CoopSalesDetails(
                     }
                     else -> {
                         currentSale?.let { sale ->
-                            SalesDetails() //hindi ko mapass ung object
+//                            InStoreSalesDetails() //hindi ko mapass ung object
+                            OnlineSalesDetails()
                         }
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+//ONLINE
+@Composable
+fun OnlineSalesDetails(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(CoopBackground)
+            .verticalScroll(rememberScrollState())
+    ) {
+        // Header Section
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(White1)
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Client Name", //sale.productName
+                    style = MaterialTheme.typography.headlineMedium,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+                Icon(
+                    Icons.Default.MoreVert,
+                    contentDescription = "More options",
+                    tint = Green1
+                )
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "OID-YYYYDDMM-Count",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+
+                Text(
+                    text = "Timestamp",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Divider(
+                color = MaterialTheme.colorScheme.onSurface,
+                thickness = 1.dp,
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+
+            // Details Section
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(White1)
+                    .padding(vertical = 8.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Details",
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+        }
+
+
+        //Product Order Details
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            colors = CardDefaults.cardColors(containerColor = White1)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                OnlineItemCard()
+                OnlineItemCard()
+
+                Text(
+                    text = "Total: PHP 200",
+                    style = MaterialTheme.typography.titleMedium,
+                )
+            }
+        }
+
+        //Collection Method
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            colors = CardDefaults.cardColors(containerColor = White1)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                CollectionMethod()
+            }
+        }
+
+        //Order Status
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(White1)
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Track Order",
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            }
+
+            //TODO: Recent ones should be on top
+            OrderStatus(
+                status = "Pending",
+                statusDescription = "Your item is to be confirmed.",
+                dateTime = "Jan 12 2025 03:20 PM",
+                isLastItem = false // Not the last item
+            )
+            OrderStatus(
+                status = "Confirmed",
+                statusDescription = "Your item is now confirmed.",
+                dateTime = "Jan 12 2025 03:30 PM",
+                isLastItem = true // Last item
+            )
+        }
+
+
+
+//        UpdateCard(){
+//
+//        }
+    }
+}
+
+
+
+
+
+
+@Composable
+fun OnlineItemCard(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = "Items (2)",
+            style = MaterialTheme.typography.titleMedium
+        )
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            colors = CardDefaults.cardColors(containerColor = White2)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    // Add Image Box
+                    Card(
+                        modifier = Modifier
+                            .size(60.dp),
+                        colors = CardDefaults.cardColors(containerColor = Color.White)
+                    ) {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text("+ Add\nImage", textAlign = TextAlign.Center, style = MaterialTheme.typography.bodySmall )
+                        }
+                    }
+
+                    // Product Name and Price
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Text(
+                            text = "Potato",
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                        Text(
+                            text = "10 kg x PHP 10",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                        Text(
+                            text = "PHP 100",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
                     }
                 }
             }
@@ -130,8 +366,102 @@ fun CoopSalesDetails(
     }
 }
 
+
 @Composable
-fun SalesDetails(){
+fun CollectionMethod(){ //Online
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = "Collection Method",
+            style = MaterialTheme.typography.titleMedium
+        )
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            colors = CardDefaults.cardColors(containerColor = White2)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    // Product Name and Price
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Text(
+                            text = "Pick Up",
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable // Online
+fun OrderStatus(status: String, statusDescription: String, dateTime: String, isLastItem: Boolean) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.Top
+    ) {
+        // Circle indicator only
+        Box(
+            modifier = Modifier
+                .size(12.dp)
+                .background(Color.Black, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        // Action details
+        Column(
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Text(
+                text = status,
+                style = MaterialTheme.typography.bodyMedium,
+            )
+            Text(
+                text = statusDescription,
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.Gray
+            )
+            Text(
+                text = dateTime,
+                style = MaterialTheme.typography.bodySmall,
+                color = Color.Gray
+            )
+        }
+    }
+}
+
+
+
+
+
+
+
+//INSTORE
+@Composable
+fun InStoreSalesDetails(){
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -150,7 +480,7 @@ fun SalesDetails(){
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
-                    text = "Peter Pan", //sale.productName
+                    text = "Product Name", //sale.productName
                     style = MaterialTheme.typography.headlineMedium,
                     color = MaterialTheme.colorScheme.onBackground
                 )
@@ -186,12 +516,12 @@ fun SalesDetails(){
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                PriceInfoColumn(
+                InStorePriceInfoColumn(
                     title = "Selling Price",
                     price = 1.00,
                     weightUnit = "weight unit"
                 )
-                PriceInfoColumn(
+                InStorePriceInfoColumn(
                     title = "Purchase Cost",
                     price = 1.00,
                     weightUnit = "weight unit"
@@ -226,13 +556,13 @@ fun SalesDetails(){
                 color = MaterialTheme.colorScheme.onSurfaceVariant
             )
         }
-        DetailsCard()
+        InStoreDetailsCard()
         NotesCard()
     }
 }
 
 @Composable
-private fun DetailsCard(){
+private fun InStoreDetailsCard(){
     // Items Section
     Column(
         modifier = Modifier
@@ -303,8 +633,32 @@ private fun DetailsCard(){
 }
 
 @Composable
+private fun InStorePriceInfoColumn(
+    title: String,
+    price: Double,
+    weightUnit: String
+) {
+    Column {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+        Text(
+            text = "PHP ",
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+        Text(
+            text = "per ",
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+    }
+}
+
+@Composable
 private fun NotesCard(){
-    // Items Section
     Column(
         modifier = Modifier
             .fillMaxWidth(),
@@ -329,7 +683,6 @@ private fun NotesCard(){
                 )
                 Divider(color = Green4, thickness = 1.dp)
 
-                // Item Details
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween
@@ -343,30 +696,5 @@ private fun NotesCard(){
                 }
             }
         }
-    }
-}
-
-@Composable
-private fun PriceInfoColumn(
-    title: String,
-    price: Double,
-    weightUnit: String
-) {
-    Column {
-        Text(
-            text = title,
-            style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onSurfaceVariant
-        )
-        Text(
-            text = "PHP ",
-            style = MaterialTheme.typography.titleLarge,
-            color = MaterialTheme.colorScheme.onBackground
-        )
-        Text(
-            text = "per ",
-            style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onSurfaceVariant
-        )
     }
 }
